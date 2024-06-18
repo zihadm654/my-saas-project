826b97b20188fe98c99290c6ca65ed3ca63d7ac3
Abdul Malek <zihadm654@gmail.com>
2024-06-18 21:34:16 +0600

    progressive web app setup configured

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@
 const { withContentlayer } = require("next-contentlayer2");
 
 import("./env.mjs");
-
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -30,4 +33,16 @@ const nextConfig = {
   },
 };
 
-module.exports = withContentlayer(nextConfig);
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withPWA = require("@ducanh2912/next-pwa").default({
+      dest: "public",
+      disable: process.env.NODE_ENV === "development",
+      register: true,
+      scope: "/app",
+      sw: "service-worker.js",
+    });
+    return withPWA(withContentlayer(nextConfig));
+  }
+  return nextConfig;
+};
